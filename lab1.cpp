8bb6069718ccb7f27c4dc9d65f984b2ba6e19354
Jason <jjellie1@csub.edu>
2020-08-27 12:13:42 -0700

    Added automated test

--- lab1.cpp
+++ lab1.cpp
@@ -5,33 +5,56 @@
 
 bool check(std::string a);
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-    //Declaring Variables
-    bool doubleLetter = false;
-    //char word[20];
-    std::string word = "";
+	//Declaring Variables
+	bool doubleLetter = false;
+	bool quit = false;
+	std::string word = ""; 
+	std::string words[6] = {"a", "aardvark", "book", "fall", "abcdddddd", "test"};
+	char choice;
 
-    std::cout << "This code will find words with double letters.\n";
-    do {
-        //Receiving user input
-        std::cout << "Enter a word: ";
-        //Using getline instead of just cin to add support for searching an entire sentence for double letters. 
-        getline(std::cin, word);        
-        //Calling function to search for double letters.
-        doubleLetter = check(word);
-        if (!doubleLetter) std::cout << word << " does not have double letters.\n";
-    } while (doubleLetter == false);
-    std::cout << word << " has double letters.\n";
-
-    return 0;
+	std::cout << "This code will find words with double letters.\n";
+	while (!quit) {
+	std::cout << "\nAutomated: A/a\nEnter word: E/e\nQuit: Q/q\n";
+	std::cin >> choice;
+	std::cout << "\n";
+		switch (choice) {
+			case 'Q':
+			case 'q':
+				quit = true;
+				break;
+			case 'A':
+			case 'a':
+				for(int x = 0; x < 6; x++) {
+					std::cout << words[x];
+					(check(words[x]) ? std::cout << " has double letters.\n" : std::cout << " does not have double letters.\n");
+				}
+				break;
+			case 'E':
+			case 'e':
+				do {
+					//Receiving user input
+					std::cout << "Enter a word: ";
+					std::cin >> word;        
+					//Calling function to search for double letters.
+					doubleLetter = check(word);
+					if (!doubleLetter) std::cout << word << " does not have double letters.\n";
+				} while (doubleLetter == false);
+				std::cout << word << " has double letters.\n";
+				break;
+			default: std::cout << "Please enter a valid option\n";
+				 break;
+		}
+	}
+	return 0;
 }
 
 bool check(std::string a) 
 {
-    //Looping through array until null terminator to search for double letters;
-    for (int x = 1; a[x] != '\0'; x++) {
-        if (a[x] == a[x - 1]) return true;
-    } 
-    return false;
+	//Looping through array until null terminator to search for double letters;
+	for (int x = 1; a[x] != '\0'; x++) {
+		if (a[x] == a[x - 1]) return true;
+	} 
+	return false;
 }
